a2d351b4bc587dbac71ab36f5807620f7e1b7ebd
lexon.wang <lexon.wang@gmail.com>
2026-03-02 10:56:06 +0800

    test

--- main.tf
+++ main.tf
@@ -4,39 +4,39 @@ provider "aws" {
   # region = "us-east-1"
 }
 
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "5.1.0"
+module "vpc-test" {
+  source = "git::https://github.com/terraform-aws-modules/terraform-aws-vpc.git?ref=v5.1.0"
+
 
   # VPC Configuration
-  name                 = var.name
-  cidr                 = var.cidr
-  enable_dns_hostnames = var.enable_dns_hostnames
-  enable_dns_support   = var.enable_dns_support
+  name                  = var.name
+  cidr                  = var.cidr
+  enable_dns_hostnames  = var.enable_dns_hostnames
+  enable_dns_support    = var.enable_dns_support
   instance_tenancy     = var.instance_tenancy
 
   # Availability Zones
   azs = var.azs
 
   # Public Subnets
-  public_subnets          = var.public_subnets
+  public_subnets        = var.public_subnets
   map_public_ip_on_launch = var.map_public_ip_on_launch
 
   # Private Subnets
-  private_subnets = var.private_subnets
+  private_subnets       = var.private_subnets
 
   # Database Subnets
-  database_subnets                   = var.database_subnets
+  database_subnets                  = var.database_subnets
   create_database_subnet_route_table = var.create_database_subnet_route_table
 
   # NAT Gateway
-  enable_nat_gateway     = var.enable_nat_gateway
-  single_nat_gateway     = var.single_nat_gateway
+  enable_nat_gateway    = var.enable_nat_gateway
+  single_nat_gateway   = var.single_nat_gateway
   one_nat_gateway_per_az = var.one_nat_gateway_per_az
-  nat_gateway_tags       = var.nat_gateway_tags
+  nat_gateway_tags     = var.nat_gateway_tags
 
   # VPN Gateway
-  enable_vpn_gateway = var.enable_vpn_gateway
+  enable_vpn_gateway   = var.enable_vpn_gateway
 
   # DHCP Options
   enable_dhcp_options              = var.enable_dhcp_options
